d995921706f11f3974609263be4b71c09613f836
trixter20716 <33231037+trixter20716@users.noreply.github.com>
2017-10-30 13:22:07 -0400

    Main File

---
+++ HiddenWord.java
@@ -0,0 +1,20 @@
+public class HiddenWord
+{
+	private String word;
+
+	public HiddenWord(String h)
+	{
+		word=h;
+	}
+
+	public String getHint(String g)
+	{
+		String hint;
+
+		for (int i=0; i<g.length()-1; i++)
+		{
+			if (g.charAt(i)==word.charAt(i))
+			{
+				word += g.charAt(i);
+			}
+			else if (g.charA
