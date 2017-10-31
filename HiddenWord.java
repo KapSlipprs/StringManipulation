962310d777f7796a5674ef78225923f4489e04ff
KapSlipprs <33231013+KapSlipprs@users.noreply.github.com>
2017-10-31 13:30:30 -0400

    Test1

--- HiddenWord.java
+++ HiddenWord.java
@@ -1,23 +1,38 @@
 public class HiddenWord
 {
 
-
+	
 	private String word;
 
 	public HiddenWord(String h)
 	{
-		word=h;
+		word = h;
 	}
 
 	public String getHint(String g)
 	{
 		String words = "";
-
+		
+		
 		for(int c = 0; c < words.length();  c++)
 		{
-
+			String letter = g.substring(c,c+1);
+		
+			if(g.charAt(c) == word.charAt(c))
+			{
+				 words += letter;
+			}
+			else if (g.indexOf(letter) > -1) 
+			{
+				words += "+";
+			}
+			else 
+			{
+				words += "*";
+			}
 
 		}
+		
 		return words;
 	}
 }
