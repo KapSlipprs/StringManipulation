cef6f1b9abcb34df9bd143b4254dc363b9814f14
trixter20716 <33231037+trixter20716@users.noreply.github.com>
2017-10-31 13:41:59 -0400

    word game stuff

---
+++ WordGame.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class WordGame
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+
+		int wordNumber = (int)(Math.random()*10);
+
+		String[] words={"honda","third","truck","laugh","silly","shake","color","scene","books","candy"};
+		String word = words[wordNumber];
+		String guess;
+
+		HiddenWord puzzle=new HiddenWord(word);
+
+		do
+		{
+			System.out.print("What is your guess? ");
+			guess= keyboard.nextLine();
+
+			if(guess.equals(puzzle.getHint(guess)))
+			{
+				System.out.println("Oh no! That isn't correct!");
+				System.out.println("Your hint is: "+puzzle.getHint(guess));
+			}
+		}
+		while(!(guess.equals(puzzle.getHint(guess))));
+		System.out.println("Good Job!you got it correct!");
+		System.out.println("Your word was: "+word);
+
+
+	}
+}
